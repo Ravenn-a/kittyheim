5a859ab8048ae1b731b45fe2dac1eb761b8883f9
Ravenn-a <rvictgiottic@gmail.com>
2026-04-02 08:48:30 -0300

    added alerts (still not perfect)

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function toggleMode() {
     const html = document.documentElement
-
     html.classList.toggle('light')
+
+    const img = document.getElementsByClassName('speech');
+    alert ('meow!');
+
+    const msg = document.getElementById('mode-message');
+    if (html.classList.contains('light')) {
+        alert ('All hail the sun god...'); 
+    } else { 
+        alert ('Midnight comes...');
+    }
 }
+
